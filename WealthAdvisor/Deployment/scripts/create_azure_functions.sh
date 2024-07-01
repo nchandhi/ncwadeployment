1db30b3c2f52f15d106e909a435d8109240a02e3
Nalini Chandhi <nalini.chandhi@microsoft.com>
2024-06-30 23:14:38 -0700

    fn changes

--- WealthAdvisor/Deployment/scripts/create_azure_functions.sh
+++ WealthAdvisor/Deployment/scripts/create_azure_functions.sh
@@ -34,11 +34,11 @@ az functionapp create --resource-group $resourceGroupName --name $functionappnam
                 --functions-version 4 --runtime python \
                 --image nctestcontainerreg2.azurecr.io/ncazfunctionsimage:v1.0.0
 
-az functionapp config appsettings set --name $functionappname -g $resourceGroupName \
-                --settings AZURE_OPEN_AI_API_KEY=$azureOpenAIApiKey AZURE_OPEN_AI_DEPLOYMENT_MODEL=$azureOpenAIDeploymentModel \
-                AZURE_OPEN_AI_ENDPOINT=$azureOpenAIEndpoint AZURE_OPENAI_EMBEDDING_DEPLOYMENT=$azureOpenAIEmbeddingDeployment \
-                OPENAI_API_VERSION=$azureOpenAIApiVersion \
-                AZURE_AI_SEARCH_API_KEY=$azureOpenAIApiKey AZURE_AI_SEARCH_ENDPOINT=$azureSearchServiceEndpoint \
-                AZURE_SEARCH_INDEX=$azureSearchIndex \
-                PYTHON_ENABLE_INIT_INDEXING="1" PYTHON_ISOLATE_WORKER_DEPENDENCIES="1" \
-                SQLDB_CONNECTION_STRING=$sqlDBConn
+# az functionapp config appsettings set --name $functionappname -g $resourceGroupName \
+#                 --settings AZURE_OPEN_AI_API_KEY=$azureOpenAIApiKey AZURE_OPEN_AI_DEPLOYMENT_MODEL=$azureOpenAIDeploymentModel \
+#                 AZURE_OPEN_AI_ENDPOINT=$azureOpenAIEndpoint AZURE_OPENAI_EMBEDDING_DEPLOYMENT=$azureOpenAIEmbeddingDeployment \
+#                 OPENAI_API_VERSION=$azureOpenAIApiVersion \
+#                 AZURE_AI_SEARCH_API_KEY=$azureOpenAIApiKey AZURE_AI_SEARCH_ENDPOINT=$azureSearchServiceEndpoint \
+#                 AZURE_SEARCH_INDEX=$azureSearchIndex \
+#                 PYTHON_ENABLE_INIT_INDEXING="1" PYTHON_ISOLATE_WORKER_DEPENDENCIES="1" \
+#                 SQLDB_CONNECTION_STRING=$sqlDBConn
